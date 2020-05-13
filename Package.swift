561e371573b6e4270f56051f396df12b65912fc3
Sergey Kazakov <kazaimazai@gmail.com>
2020-05-13 20:26:30 +0300

    updated dependencies

--- Package.swift
+++ Package.swift
@@ -13,16 +13,24 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VaporRestKit",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent")
+        ]),
         .testTarget(
             name: "VaporRestKitTests",
-            dependencies: ["VaporRestKit"]),
+            dependencies: [
+                .target(name: "VaporRestKit"),
+                .product(name: "XCTVapor", package: "vapor"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver")
+        ]),
     ]
 )
