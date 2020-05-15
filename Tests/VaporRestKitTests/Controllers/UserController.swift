8f536a8109117fcf966c8e490558c8bd347b285e
Sergey Kazakov <kazaimazai@gmail.com>
2020-05-16 01:13:42 +0300

    updated user controller

--- Tests/VaporRestKitTests/Controllers/UserController.swift
+++ Tests/VaporRestKitTests/Controllers/UserController.swift
@@ -12,44 +12,46 @@ import Fluent
 
 struct UserControllers {
     struct AllUsersController: VersionableController {
-        let v1 = CompoundResourceController(with:[
-            User.Output
+        var v1: APIMethodsProviding {
+            return User.Output
                 .resourceController(eagerLoading: EagerLoadingUnsupported.self)
-                .create(input: User.Input.self),
+                .create(input: User.Input.self)
+                .read()
+                .update(input: User.Input.self)
+                .patch(input: User.PatchInput.self)
+                .delete()
+                .collection(sorting: SortingUnsupported.self,
+                            filtering: FilteringUnsupported.self)
 
-            User.Output
-                .resourceController(eagerLoading: EagerLoadingUnsupported.self)
-                .read(),
+        }
 
-            User.Output
-                .resourceController(eagerLoading: EagerLoadingUnsupported.self)
-                .update(input: User.Input.self),
+        var v2: APIMethodsProviding {
+                   return User.Output
+                       .resourceController(eagerLoading: EagerLoadingUnsupported.self)
+                        .relatedTo(relation: \Todo.$assignees, relationName: "assignees")
+                       .create(input: User.Input.self)
+                       .read()
+                       .update(input: User.Input.self)
+                       .patch(input: User.PatchInput.self)
+                       .delete()
+                       .collection(sorting: SortingUnsupported.self,
+                                   filtering: FilteringUnsupported.self)
 
-            User.Output
-                .resourceController(eagerLoading: EagerLoadingUnsupported.self)
-                .patch(input: User.PatchInput.self),
-
-            User.Output.resourceController(eagerLoading: EagerLoadingUnsupported.self)
-                .delete(),
-
-            User.Output
-                .resourceController(eagerLoading: EagerLoadingUnsupported.self)
-                .collection(sorting: SortingUnsupported.self, filtering: FilteringUnsupported.self)
-
-        ])
-
-        let v2 = CompoundResourceController(with:[
-            User.OutputV2
-                .resourceController(eagerLoading: EagerLoadingUnsupported.self)
-                .read(),
-
-            User.OutputV2
-                .resourceController(eagerLoading: EagerLoadingUnsupported.self)
-                .collection(sorting: SortingUnsupported.self, filtering: FilteringUnsupported.self)
+               }
 
 
-
-        ])
+        //        let v2 = CompoundResourceController(with:[
+        //            User.OutputV2
+        //                .resourceController(eagerLoading: EagerLoadingUnsupported.self)
+        //                .read(),
+        //
+        //            User.OutputV2
+        //                .resourceController(eagerLoading: EagerLoadingUnsupported.self)
+        //                .collection(sorting: SortingUnsupported.self, filtering: FilteringUnsupported.self)
+        //
+        //
+        //
+        //        ])
 
 
         func setupAPIMethods(on routeBuilder: RoutesBuilder, for endpoint: String, with version: ApiVersion) {
@@ -57,7 +59,7 @@ struct UserControllers {
             case .v1:
                 v1.addMethodsTo(routeBuilder, on: endpoint)
             case .v2:
-                v2.addMethodsTo(routeBuilder, on: endpoint)
+                v1.addMethodsTo(routeBuilder, on: endpoint)
             }
         }
     }
