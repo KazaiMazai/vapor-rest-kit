fe36e665d04b199bf5e0260931f83bedba30c66a
Sergey Kazakov <kazaimazai@gmail.com>
2020-05-15 19:38:29 +0300

    sibling model updated to support different types of From and To ID Types

--- Sources/VaporRestKit/FluentExtensions/Model/SiblingModel.swift
+++ Sources/VaporRestKit/FluentExtensions/Model/SiblingModel.swift
@@ -28,9 +28,10 @@ public extension SiblingRepresentable {
 //MARK:- SiblingModel
 
 public final class SiblingModel<From, To, Name>: Model, Content
-    where From: Fluent.Model,
-        To: Fluent.Model,
-        Name: SiblingRepresentable {
+    where
+    From: Fluent.Model,
+    To: Fluent.Model,
+    Name: SiblingRepresentable {
 
     public static var schema: String {
         return "\(From.schema)_\(To.schema)_\(Name.siblingName)_pivot"
@@ -65,14 +66,87 @@ extension SiblingModel: FieldsProvidingModel {
 
 //MARK:- SiblingModel + InitMigratableSchema
 
-extension SiblingModel: InitMigratableSchema where From.IDValue == UUID, To.IDValue == UUID  {
+extension SiblingModel: InitMigratableSchema {
     public static func prepare(on schemaBuilder: SchemaBuilder) -> EventLoopFuture<Void> {
+        guard let fromIdType = DatabaseSchema.DataType(type: From.IDValue.self) else {
+            return schemaBuilder.create()
+                                .eventLoop
+                                .makeFailedFuture(FluentError.invalidField(name: "\(From.schema).id",
+                                    valueType: From.IDValue.self,
+                                    error: SiblingModelError.idTypeUnsupported))
+        }
+
+
+        guard let toIdType = DatabaseSchema.DataType(type: From.IDValue.self) else {
+            return schemaBuilder.create()
+                                .eventLoop
+                                .makeFailedFuture(FluentError.invalidField(name: "\(To.schema).id",
+                                    valueType: To.IDValue.self,
+                                    error: SiblingModelError.idTypeUnsupported))
+        }
+
         return schemaBuilder
             .id()
-            .field(Fields.fromId.key, .uuid, .required, .references(From.schema, .id))
-            .field(Fields.toId.key, .uuid, .required, .references(To.schema, .id))
+            .field(Fields.fromId.key, fromIdType, .required, .references(From.schema, .id))
+            .field(Fields.toId.key, toIdType, .required, .references(To.schema, .id))
             .create()
     }
 }
 
 
+fileprivate extension DatabaseSchema.DataType {
+    init?<T>(type: T.Type) {
+        switch type.self {
+        case is UUID.Type:
+            self = .uuid
+        case is Bool.Type:
+            self = .bool
+        case is Int.Type:
+            self = .int
+        case is Int8.Type:
+            self = .int8
+        case is Int16.Type:
+            self = .int16
+        case is Int32.Type:
+            self = .int32
+        case is Int64.Type:
+            self = .int64
+        case is UInt.Type:
+            self = .uint
+        case is UInt8.Type:
+            self = .uint8
+        case is UInt16.Type:
+            self = .uint16
+        case is UInt32.Type:
+            self = .uint32
+        case is UInt64.Type:
+            self = .uint64
+        case is Float.Type:
+            self = .float
+        case is Double.Type:
+            self = .double
+        case  is String.Type:
+            self = .string
+        case is Date.Type:
+            self = .date
+        default:
+            return nil
+        }
+    }
+}
+
+
+fileprivate enum SiblingModelError: Error, LocalizedError, CustomStringConvertible {
+    case idTypeUnsupported
+
+    var description: String {
+        switch self {
+        case .idTypeUnsupported:
+            return "ID type is not supported"
+        }
+    }
+
+    var errorDescription: String? {
+        return self.description
+    }
+}
