61d69649608fd9ec1ec6226bee3b2e42165d8b85
Sergey Kazakov <kazaimazai@gmail.com>
2020-05-17 15:02:09 +0300

    removed unused code

--- Sources/VaporRestKit/ResourceModelProtocols.swift
+++ Sources/VaporRestKit/ResourceModelProtocols.swift
@@ -26,40 +26,10 @@ public protocol ResourcePatchModel: Content, Validatable {
     func patch(_: Model, req: Request, database: Database) -> EventLoopFuture<Model>
 }
 
-//public protocol ResourceDeleteHandler {
-//    associatedtype Model: Fluent.Model
-//
-//    func delete(_: Model, req: Request, database: Database) -> EventLoopFuture<Model>
-//}
-//
-//extension ResourceDeleteHandler {
-//    func delete(_ model: Model, req: Request, database: Database) -> EventLoopFuture<Model> {
-//        return model.delete(on: database).transform(to: model)
-//    }
-//}
-
-//struct PlainDelete<Model: Fluent.Model>: ResourceDeleteHandler {
-//    static func validations(_ validations: inout Validations) { }
-//
-//}
-
 struct SuccessOutput<Model: Fields>: ResourceOutputModel {
     let success = true
 
     init(_ model: Model, req: Request) {  }
-}
-
-
-public struct Deleter<Model: Fluent.Model> {
-    fileprivate let handler: (Model, Request, Database) -> EventLoopFuture<Model>
-
-    public func delete(_ model: Model, req: Request, database: Database) -> EventLoopFuture<Model> {
-        return handler(model, req, database)
-    }
-
-    public static var defaultDeleter: Deleter<Model> {
-        return Deleter(handler: { model, _, db in model.delete(on: db).transform(to: model) })
-    }
 }
 
 public struct ResourceMiddleware<Model: Fluent.Model> {
@@ -70,7 +40,6 @@ public struct ResourceMiddleware<Model: Fluent.Model> {
     public init(willSave: @escaping Handler = Self.empty) {
         self.willSaveHandler = willSave
     }
-
 
     public func willSave(_ model: Model,
                          req: Request,
