d1038d963f1f419a5bd0b80bdeadbac87eb7fc2c
Sergey Kazakov <kazaimazai@gmail.com>
2021-08-14 18:14:42 +0300

    resource paths refactoring

--- Sources/VaporRestKit/Resources+Helpers/Resources+Paths.swift
+++ Sources/VaporRestKit/Resources+Helpers/Resources+Paths.swift
@@ -8,46 +8,122 @@
 import Vapor
 import Fluent
 
-extension Model where IDValue: LosslessStringConvertible {
-    static var idKey: String { Self.schema }
-    static var idPathComponent: PathComponent { return PathComponent(stringLiteral: ":\(self.idKey)") }
 
-    static func resourcePathFor(endpoint: String) -> [PathComponent] {
+extension Array where Element == PathComponent {
+    static func pathFor<Model>(_ model: Model.Type,
+                               on endpoint: String = Model.pluralPath) -> [PathComponent]
+    where
+        Model: Fluent.Model,
+        Model.IDValue: LosslessStringConvertible {
+
         let endpointPath = PathComponent(stringLiteral: endpoint)
         return [endpointPath]
     }
 
-    static func idResourcePathFor(endpoint: String) -> [PathComponent] {
+    static func idPathFor<Model>(_ model: Model.Type,
+                                         on endpoint: String = Model.pluralPath) -> [PathComponent]
+    where
+        Model: Fluent.Model,
+        Model.IDValue: LosslessStringConvertible {
+
         let endpointPath = PathComponent(stringLiteral: endpoint)
-        return [endpointPath, idPathComponent]
+        return [endpointPath, Model.idPath]
+    }
+}
+
+extension Array where Element == PathComponent {
+    static func pathFor<Model, RelatedModel>(_ model: Model.Type,
+                                                            relationPath: String? = nil,
+                                                            relatedModel: RelatedModel.Type,
+                                                            on endpoint: String = Model.pluralPath) -> [PathComponent] where
+        RelatedModel: Fluent.Model,
+        Model: Fluent.Model,
+        RelatedModel.IDValue: LosslessStringConvertible,
+        Model.IDValue: LosslessStringConvertible {
+
+        let endpointPath = PathComponent(stringLiteral: endpoint)
+        let relationPathComponent = relationPath.map { PathComponent(stringLiteral: "\($0)")}
+        return [RelatedModel.idPath, relationPathComponent, endpointPath].compactMap { $0 }
+    }
+
+    static func idPathFor<Model, RelatedModel>(_ model: Model.Type,
+                                                              relationPath: String? = nil,
+                                                              relatedModel: RelatedModel.Type,
+                                                              on endpoint: String = Model.pluralPath) -> [PathComponent]  where
+        RelatedModel: Fluent.Model,
+        Model: Fluent.Model,
+        RelatedModel.IDValue: LosslessStringConvertible,
+        Model.IDValue: LosslessStringConvertible {
+
+        let relationPathComponent = relationPath.map { PathComponent(stringLiteral: "\($0)")}
+        let endpointPath = PathComponent(stringLiteral: endpoint)
+        return [RelatedModel.idPath, relationPathComponent, endpointPath, Model.idPath].compactMap { $0 }
+    }
+
+    static func authPathFor<Model, RelatedModel>(_ model: Model.Type,
+                                                            relationPath: String? = nil,
+                                                            relatedModel: RelatedModel.Type,
+                                                            on endpoint: String = Model.pluralPath) -> [PathComponent] where
+        RelatedModel: Fluent.Model,
+        Model: Fluent.Model,
+        RelatedModel.IDValue: LosslessStringConvertible,
+        Model.IDValue: LosslessStringConvertible {
+
+        let endpointPath = PathComponent(stringLiteral: endpoint)
+        let relationPathComponent = relationPath.map { PathComponent(stringLiteral: "\($0)")}
+        return ["me", relationPathComponent, endpointPath].compactMap { $0 }
+    }
+
+    static func authIdPathFor<Model, RelatedModel>(_ model: Model.Type,
+                                                              relationPath: String? = nil,
+                                                              relatedModel: RelatedModel.Type,
+                                                              on endpoint: String = Model.pluralPath) -> [PathComponent]  where
+        RelatedModel: Fluent.Model,
+        Model: Fluent.Model,
+        RelatedModel.IDValue: LosslessStringConvertible,
+        Model.IDValue: LosslessStringConvertible {
+
+        let relationPathComponent = relationPath.map { PathComponent(stringLiteral: "\($0)")}
+        let endpointPath = PathComponent(stringLiteral: endpoint)
+        return ["me", relationPathComponent, endpointPath, Model.idPath].compactMap { $0 }
     }
 }
 
 extension Model where IDValue: LosslessStringConvertible {
+    static var idKey: String { Self.schema }
+
+    static var pluralPath: String { "\(self.idKey)s"}
+
+
+    static var resourcePath: PathComponent { PathComponent(stringLiteral: pluralPath) }
+    static var idPath: PathComponent { return PathComponent(stringLiteral: ":\(self.idKey)") }
+}
+
+extension Model where IDValue: LosslessStringConvertible {
     static func relatedResourcePathFor<RelatedModel>(_ model: RelatedModel.Type,
-                                            relationPath: String? = nil,
-                                            on endpoint: String) -> [PathComponent] where
+                                                     relationPath: String? = nil,
+                                                     on endpoint: String) -> [PathComponent] where
         RelatedModel: Fluent.Model,
         RelatedModel.IDValue: LosslessStringConvertible {
 
         let endpointPath = PathComponent(stringLiteral: endpoint)
         let relationPathComponent = relationPath.map { PathComponent(stringLiteral: "\($0)")}
-        return [RelatedModel.idPathComponent, relationPathComponent, endpointPath].compactMap { $0 }
+        return [RelatedModel.idPath, relationPathComponent, endpointPath].compactMap { $0 }
     }
 
     static func relatedIdResourcePathFor<RelatedModel>(_ model: RelatedModel.Type,
-                                              relationPath: String? = nil,
-                                              on endpoint: String) -> [PathComponent]  where
+                                                       relationPath: String? = nil,
+                                                       on endpoint: String) -> [PathComponent]  where
         RelatedModel: Fluent.Model,
         RelatedModel.IDValue: LosslessStringConvertible {
         let relationPathComponent = relationPath.map { PathComponent(stringLiteral: "\($0)")}
         let endpointPath = PathComponent(stringLiteral: endpoint)
-        return [RelatedModel.idPathComponent, relationPathComponent, endpointPath, Self.idPathComponent].compactMap { $0 }
+        return [RelatedModel.idPath, relationPathComponent, endpointPath, Self.idPath].compactMap { $0 }
     }
 
     static func authRelatedResourcePathFor<RelatedModel>(_ model: RelatedModel.Type,
-                                            relationPath: String? = nil,
-                                            on endpoint: String) -> [PathComponent] where
+                                                         relationPath: String? = nil,
+                                                         on endpoint: String) -> [PathComponent] where
         RelatedModel: Fluent.Model,
         RelatedModel.IDValue: LosslessStringConvertible {
 
@@ -57,12 +133,12 @@ extension Model where IDValue: LosslessStringConvertible {
     }
 
     static func authRelatedIdResourcePathFor<RelatedModel>(_ model: RelatedModel.Type,
-                                              relationPath: String? = nil,
-                                              on endpoint: String) -> [PathComponent]  where
+                                                           relationPath: String? = nil,
+                                                           on endpoint: String) -> [PathComponent]  where
         RelatedModel: Fluent.Model,
         RelatedModel.IDValue: LosslessStringConvertible {
         let relationPathComponent = relationPath.map { PathComponent(stringLiteral: "\($0)")}
         let endpointPath = PathComponent(stringLiteral: endpoint)
-        return ["me", relationPathComponent, endpointPath, Self.idPathComponent].compactMap { $0 }
+        return ["me", relationPathComponent, endpointPath, Self.idPath].compactMap { $0 }
     }
 }
