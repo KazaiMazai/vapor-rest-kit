4376ddac8cbafececd0128e3bb970ff2e5ca50b5
Sergey Kazakov <kazaimazai@gmail.com>
2024-06-23 17:04:30 +0100

    resource mutation model update and concurrency support

--- Sources/VaporRestKit/ResourceModels/ResourcePatchModel.swift
+++ Sources/VaporRestKit/ResourceModels/ResourcePatchModel.swift
@@ -11,20 +11,24 @@ import Fluent
 public protocol ResourcePatchModel: ResourceMutationModel {
     func patch(_: Model) throws -> Model
 
+    func patch(_ model: Model, req: Request, database: Database) async throws -> Model
+    
     func patch(_ model: Model, req: Request, database: Database) -> EventLoopFuture<Model>
 }
 
 public extension ResourcePatchModel {
+    func patch(_ model: Model, req: Request, database: Database) async throws -> Model {
+        try await req.eventLoop.tryFuture { try patch(model) }.get()
+    }
+    
     func patch(_ model: Model, req: Request, database: Database) -> EventLoopFuture<Model> {
-        return req.eventLoop.tryFuture { try patch(model) }
+        req.eventLoop.withTask {
+            try await patch(model, req: req, database: database)
+        }
     }
 }
 
 public extension ResourcePatchModel {
-    func mutate(_ model: Model) throws -> Model {
-        try patch(model)
-    }
-
     func mutate(_ model: Model, req: Request, database: Database) -> EventLoopFuture<Model> {
         patch(model, req: req, database: database)
     }
