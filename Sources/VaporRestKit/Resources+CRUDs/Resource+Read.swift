f5dc42f36db0071cb22126a01d489d5e50eaa19d
Serge Kazakov <kazaimazai@gmail.com>
2026-01-11 00:22:04 +0000

    added async method for resource read with resolver (#51)

--- Sources/VaporRestKit/Resources+CRUDs/Resource+Read.swift
+++ Sources/VaporRestKit/Resources+CRUDs/Resource+Read.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Sergey Kazakov on 08.08.2021.
 //
@@ -13,11 +13,11 @@ public extension ResourceController {
                      resolver: Resolver<Model> = .byIdKeys,
                      queryModifier: QueryModifier<Model> = .empty) throws -> EventLoopFuture<Output>
     where
-        Output.Model == Model {
-            
-            try resolver
-                .find(req, req.db, queryModifier)
-                .flatMapThrowing { model in try Output(model, req: req) }
+    Output.Model == Model {
+        
+        try resolver
+            .find(req, req.db, queryModifier)
+            .flatMapThrowing { model in try Output(model, req: req) }
     }
 }
 
@@ -28,34 +28,34 @@ public extension RelatedResourceController {
         queryModifier: QueryModifier<Model> = .empty,
         relationKeyPath: ChildrenKeyPath<RelatedModel, Model>) throws -> EventLoopFuture<Output>
     where
-        Model == Output.Model {
-
+    Model == Output.Model {
+        
         try resolver
             .find(req, req.db, relationKeyPath, queryModifier)
             .flatMapThrowing { (model, related) in try Output(model, req: req) }
     }
-
+    
     func read<Model, RelatedModel>(
         resolver: ParentResolver<Model, RelatedModel> = .byIdKeys,
         req: Request,
         queryModifier: QueryModifier<Model> = .empty,
         relationKeyPath: ChildrenKeyPath<Model, RelatedModel>) throws -> EventLoopFuture<Output>
     where
-        Model == Output.Model {
-
+    Model == Output.Model {
+        
         try resolver
             .find(req, req.db, relationKeyPath, queryModifier)
             .flatMapThrowing { (model, related) in try Output(model, req: req)}
     }
-
+    
     func read<Model, RelatedModel, Through>(
         resolver: SiblingsResolver<Model, RelatedModel, Through> = .byIdKeys,
         req: Request,
         queryModifier: QueryModifier<Model> = .empty,
         relationKeyPath: SiblingKeyPath<RelatedModel, Model, Through>) throws -> EventLoopFuture<Output>
     where
-        Model == Output.Model {
-
+    Model == Output.Model {
+        
         try resolver
             .find(req, req.db, relationKeyPath, queryModifier)
             .flatMapThrowing { (model, related) in try Output(model, req: req) }
@@ -66,14 +66,16 @@ public extension RelatedResourceController {
 
 public extension ResourceController {
     func read<Model>(req: Request,
-                      queryModifier: QueryModifier<Model> = .empty) async throws -> Output
+                     resolver: Resolver<Model> = .byIdKeys,
+                     queryModifier: QueryModifier<Model> = .empty) async throws -> Output
     where
-        Output.Model == Model {
-
-            try await read(
-                req: req,
-                queryModifier: queryModifier
-            ).get()
+    Output.Model == Model {
+        
+        try await read(
+            req: req,
+            resolver: resolver,
+            queryModifier: queryModifier
+        ).get()
     }
 }
 
@@ -84,46 +86,46 @@ public extension RelatedResourceController {
         queryModifier: QueryModifier<Model> = .empty,
         relationKeyPath: ChildrenKeyPath<RelatedModel, Model>) async throws -> Output
     where
-        Model == Output.Model {
-
-            try await read(
-                resolver: resolver,
-                req: req,
-                queryModifier: queryModifier,
-                relationKeyPath: relationKeyPath
-            ).get()
+    Model == Output.Model {
+        
+        try await read(
+            resolver: resolver,
+            req: req,
+            queryModifier: queryModifier,
+            relationKeyPath: relationKeyPath
+        ).get()
     }
-
+    
     func read<Model, RelatedModel>(
         resolver: ParentResolver<Model, RelatedModel> = .byIdKeys,
         req: Request,
         queryModifier: QueryModifier<Model> = .empty,
         relationKeyPath: ChildrenKeyPath<Model, RelatedModel>) async throws -> Output
     where
-        Model == Output.Model {
-
-            try await read(
-                resolver: resolver,
-                req: req,
-                queryModifier: queryModifier,
-                relationKeyPath: relationKeyPath
-            ).get()
-        }
-
+    Model == Output.Model {
+        
+        try await read(
+            resolver: resolver,
+            req: req,
+            queryModifier: queryModifier,
+            relationKeyPath: relationKeyPath
+        ).get()
+    }
+    
     func read<Model, RelatedModel, Through>(
         resolver: SiblingsResolver<Model, RelatedModel, Through> = .byIdKeys,
         req: Request,
         queryModifier: QueryModifier<Model> = .empty,
         relationKeyPath: SiblingKeyPath<RelatedModel, Model, Through>) async throws -> Output
     where
-        Model == Output.Model {
-
-            try await read(
-                resolver: resolver,
-                req: req,
-                queryModifier: queryModifier,
-                relationKeyPath: relationKeyPath
-            ).get()
+    Model == Output.Model {
+        
+        try await read(
+            resolver: resolver,
+            req: req,
+            queryModifier: queryModifier,
+            relationKeyPath: relationKeyPath
+        ).get()
     }
 }
 
